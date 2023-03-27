76d7afe36fdd338f4facd81886da57e0a4081a19
estudoSocial <128539304+estudoSocial@users.noreply.github.com>
2023-03-27 15:37:43 -0300

    manager user.

--- app.py
+++ app.py
@@ -30,9 +30,63 @@ if escola_logada:
     procedimento = form.selectbox('Procedimento', options=['Entrada', 'Saída'])
     form.form_submit_button('Registrar')
 
-if produto and quantidade and procedimento:
-    merenda.registrar(conn, escola_logada, produto, unidade, quantidade, procedimento)
+    if produto and quantidade and procedimento:
+        merenda.registrar(conn, escola_logada, produto, unidade, quantidade, procedimento)
 
-st.subheader(f'Histórico de Registros ({escola_logada})')
-df = merenda.list_records(conn, escola_logada)
-st.dataframe(df)
+    st.subheader(f'Histórico de Registros ({escola_logada})')
+    df = merenda.list_records(conn, escola_logada)
+    st.dataframe(df)
+    
+    if escola_logada == "SEDUC":
+        st.subheader('Gerenciar Usuários')
+
+        user_management_form = st.form(key='user_management')
+        user_action = user_management_form.radio('Ação', options=['Criar', 'Editar', 'Excluir'])
+        target_user = user_management_form.text_input('Nome de usuário alvo')
+        new_username = user_management_form.text_input('Novo nome de usuário (se aplicável)')
+        new_password = user_management_form.text_input('Nova senha (se aplicável)', type='password')
+        new_school = user_management_form.selectbox('Escola (se aplicável)', options=[
+            "Creche André Ricardo Perazzo da Costa",
+            "Creche Corina Barreto de Brito Lira",
+            "Creche Dona Dina",
+            "Creche Ephigênio Barbosa",
+            "Creche Ezilda Milanez",
+            "Creche José Alves do Nascimento",
+            "Escola Abel Barbosa da Silva",
+            "Escola Dr. José Inácio de Miranda Pereira",
+            "Escola João César",
+            "Escola José Lins Sobrinho",
+            "Escola José Rodrigues",
+            "Escola Júlia Verônica dos SantosLeal",
+            "Escola Madre Trautlinde",
+            "Escola Maria Emília Maracajá",
+            "Escola Nenen Silva",
+            "Escola Pedro Honório",
+            "Escola Lúcia Giovanna",
+            "Escola Ver. Nelson Carneiro",
+            "SEDUC"
+            ])
+            user_management_form.form_submit_button('Executar')
+            
+        if user_action and target_user:
+            if user_action == 'Criar':
+                if new_username and new_password and new_school:
+                    authentication.create_user(conn, new_username, new_password, new_school)
+                    st.success(f'Usuário {new_username} criado com sucesso')
+                else:
+                    st.error('Preencha todos os campos')
+            elif user_action == 'Editar':
+                if new_username or new_password or new_school:
+                    authentication.edit_user(conn, target_user, new_username, new_password, new_school)
+                    st.success(f'Usuário {target_user} editado com sucesso')
+                else:
+                    st.error('Preencha pelo menos um campo')
+            elif user_action == 'Excluir':
+                authentication.delete_user(conn, target_user)
+                st.success(f'Usuário {target_user} excluído com sucesso')
+                st.subheader('Lista de Usuários')
+                users_df = authentication.list_users(conn)
+                st.dataframe(users_df)
+            else:
+                st.error('Nome de usuário ou senha incorretos')
+
